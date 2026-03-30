3fe8fa039240271a9d14cb897cf8d3acea51631a
kodan76-creator <kodan76@gmail.com>
2026-03-30 18:01:37 +0300

    *

--- vite.config.js
+++ vite.config.js
@@ -4,34 +4,34 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Мой Универсальный Словарь',
-        short_name: 'Словарь',
-        description: 'Быстрый офлайн словарь',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
+    react()//,
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    //   manifest: {
+    //     name: 'Мой Универсальный Словарь',
+    //     short_name: 'Словарь',
+    //     description: 'Быстрый офлайн словарь',
+    //     theme_color: '#ffffff',
+    //     background_color: '#ffffff',
+    //     display: 'standalone',
+    //     icons: [
+    //       {
+    //         src: 'pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       }
+    //     ]
+    //   },
+      //workbox: {
         // Кэшируем все файлы, включая JSON словарь
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}']
-      }
-    })
+       // globPatterns: ['**/*.{js,css,html,ico,png,svg,json}']
+      //}
+    //})
   ]
 })
