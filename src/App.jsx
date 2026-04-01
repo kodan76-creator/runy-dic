f68af72fb13a0f578991c8fbb0d57d5a815b3600
kodan76-creator <kodan76@gmail.com>
2026-04-02 02:28:23 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <div className="container">
       {/* Логотип вверху слева */}
       <img 
-        src="/run_r.png" 
+        src="https://kodan76-creator.github.io/runy-dic/run_r.png" 
         alt="Logo" 
         className="logo"
         width="130"
