830c5201f0b8b9e350cbd8ecd3bbcd14c0205f0d
Denis Kostromin <kodan76@gmail.com>
2026-05-04 20:38:51 +0300

    fix app

--- src/githubApi.js
+++ src/githubApi.js
@@ -287,7 +287,7 @@ export const getLogs = async () => {
 
 export const addLog = async (logData) => {
   try {
-    // ✅ Получаем И данные, И sha ОДНИМ вызовом!
+    // ✅ ИСПРАВЛЕНО: Получаем И данные, И sha ОДНИМ вызовом!
     const {  logs, sha } = await fetchGitHubFile(LOGS_FILE)
     
     const newLog = {
